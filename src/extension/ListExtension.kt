0638453334b46ae13e44849bf7787b865a4fe36e
vkuznetsov <vkuznetsov@uma.tech>
2022-05-02 14:32:40 +0300

    refactoring code

---
+++ src/extension/ListExtension.kt
@@ -0,0 +1,25 @@
+package extension
+
+import Constants
+
+fun MutableList<MutableList<Double>>.addZeroInFirstIndex(rangeMain: IntRange, rangeSub: IntRange) {
+    for (i in rangeMain) {
+        val nullList = mutableListOf<Double>()
+        for (k in rangeSub) {
+            nullList.add(0.0)
+        }
+        this.add(nullList)
+    }
+}
+
+fun List<MutableList<Double>>.normalisation() {
+    var max = 0.0
+    this.forEach { list ->
+        if (list.maxOf { it } > max) max = list.maxOf { it }
+    }
+    for (k in 0..Constants.lll) {
+        for (i in 0..Constants.kz) {
+            this[k][i] = this[k][i] / max
+        }
+    }
+}
